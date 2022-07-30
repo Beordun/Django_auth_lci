38d618d030d7cb06988923abbe77425b297d8903
Olatunji Michael <beordunmichael@gmail.com>
2022-07-30 14:03:15 +0100

    First commit

---
+++ pro47chunk/Auth_lci/views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view_decorator
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import Authtoken
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    _,token = Authtoken.objects.create(user)
+    
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+            },
+        'token': token.key
+    })
