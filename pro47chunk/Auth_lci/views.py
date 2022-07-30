b7835a724d8f4c51893593d15487f1b04fbaccdd
Olatunji Michael <beordunmichael@gmail.com>
2022-07-31 00:11:11 +0100

    other commit

--- pro47chunk/Auth_lci/views.py
+++ pro47chunk/Auth_lci/views.py
@@ -1,15 +1,15 @@
-from rest_framework.decorators import api_view_decorator
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.auth import Authtoken
+from knox.auth import AuthToken
 
 
 @api_view(['POST'])
-def login(request):
+def login_api(request):
     serializer = AuthTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     user = serializer.validated_data['user']
-    _,token = Authtoken.objects.create(user)
+    _, token = Authtoken.objects.create(user)
     
     return Response({
         'user_info': {
--- pro47chunk/pro47chunk/settings.py
+++ pro47chunk/pro47chunk/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Auth_lci',
     'knox',
+    'Auth_lci',
 ]
 
 MIDDLEWARE = [
